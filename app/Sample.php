9da50c1c2c5cdba7d89b8759821549bd2c639a51
h.matsuo <matsuoshi@gmail.com>
2020-03-26 09:32:42 +0900

    add .editorconfig

--- app/Sample.php
+++ app/Sample.php
@@ -3,14 +3,14 @@ namespace Sample;
 
 class Sample
 {
-	/**
-  	 * 指定された日付が、対象日付の範囲内かを調べる
-  	 *
-  	 * @param int $date 対象日付
-  	 * @param int $from 開始日付
-  	 * @param int $to 終了日付
-  	 * @return bool
-	 */
+    /**
+     * 指定された日付が、対象日付の範囲内かを調べる
+     *
+     * @param int $date 対象日付
+     * @param int $from 開始日付
+     * @param int $to 終了日付
+     * @return bool
+     */
 	public function isInDateRange($date, $from, $to)
 	{
 		if ($date < $from) {
